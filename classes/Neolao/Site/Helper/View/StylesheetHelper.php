88f8a669fa3541f4d18f564e4f2f7044cd548772
neolao <contact@neolao.com>
2013-02-28 01:32:38 +0100

    View helper, stylesheet: Handle the generated URL

--- classes/Neolao/Site/Helper/View/StylesheetHelper.php
+++ classes/Neolao/Site/Helper/View/StylesheetHelper.php
@@ -14,8 +14,13 @@ use \Neolao\Site\Helper\View\AbstractHelper;
  * $helper              = new Stylesheet();
  * $helper->basePath    = '/path/to/style';
  * $helper->baseUrl     = 'http://site.tld/style';
+ *
  * $fileUrl             = $helper->getFileUrl('screen.css');
  * echo $fileUrl; // http://site.tld/style/stylesheets/screen.css
+ *
+ * $helper->generated   = true;
+ * $fileUrl             = $helper->getFileUrl('screen.css');
+ * echo $fileUrl; // http://site.tld/style/generated-style/a75ca72342d875fb6d50b03e4a6dd5a4/stylesheets/screen.css
  */
 class StylesheetHelper extends AbstractHelper
 {
@@ -32,6 +37,13 @@ class StylesheetHelper extends AbstractHelper
      * @var string
      */
     public $baseUrl = '';
+
+    /**
+     * Indicates that the stylesheet is generated
+     *
+     * @var bool
+     */
+    public $generated = false;
 
     /**
      * Get the file URL of the specified file name
@@ -52,7 +64,23 @@ class StylesheetHelper extends AbstractHelper
      */
     public function getFileUrl($fileName)
     {
-        $fileUrl = $this->baseUrl . '/stylesheets/' . $fileName;
+        // By default, use the original file
+        $fileUrl = $this->baseUrl . DIRECTORY_SEPARATOR . 'stylesheets' . DIRECTORY_SEPARATOR . $fileName;
+
+        if ($this->generated) {
+            // Use the generated file
+            $generatedDirectory = $this->basePath . DIRECTORY_SEPARATOR . 'generated-styles';
+
+            // Get the version
+            $versionPath = $generatedDirectory . DIRECTORY_SEPARATOR . 'version.txt';
+            if (!is_readable($versionPath)) {
+                return $fileUrl;
+            }
+            $version = file_get_contents($versionPath);
+
+            // Generated URL
+            $fileUrl = $this->baseUrl . DIRECTORY_SEPARATOR . 'generated-styles' . DIRECTORY_SEPARATOR . $version . DIRECTORY_SEPARATOR . 'stylesheets' . DIRECTORY_SEPARATOR . $fileName;
+        }
 
         return $fileUrl;
     }
