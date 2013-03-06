20fdd54c87e865343a4104d9774721939ad282c6
neolao <contact@neolao.com>
2013-03-06 21:35:49 +0100

    Add a behavior to serialize (and unserialize) to a json format

---
+++ classes/Neolao/Behavior/SerializableJson.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package Neolao\Behavior
+ */
+namespace Neolao\Behavior;
+
+
+/**
+ * Objects that implement this interface are serializable to json format
+ */
+interface SerializableJson
+{
+    /**
+     * Serialize to a json
+     *
+     * @return  string                  The serialized json
+     */
+    public function serializeJson();
+
+    /**
+     * Unserialize from a json
+     *
+     * @param   string      $json       The json
+     */
+    public function unserializeJson($json);
+}
