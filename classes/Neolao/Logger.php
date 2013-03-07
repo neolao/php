059b8b1953a579dbe7bbb63c7c8063f9ad57988f
neolao <contact@neolao.com>
2013-03-07 19:04:13 +0100

    Remove the dependency with Psr

--- classes/Neolao/Logger.php
+++ classes/Neolao/Logger.php
@@ -6,12 +6,11 @@ namespace Neolao;
 
 
 use \Neolao\Logger\ListenerInterface;
-use \Psr\Log\AbstractLogger;
 
 /**
  * Logger
  */
-class Logger extends AbstractLogger
+class Logger
 {
     use \Neolao\Mixin\Singleton;
 
@@ -71,5 +70,94 @@ class Logger extends AbstractLogger
             $listener->log($level, $generatedMessage);
         }
     }
+
+    /**
+     * System is unusable
+     *
+     * @param   string      $message    The message
+     * @param   array       $context    The context
+     */
+    public function emergency($message, array $context = [])
+    {
+        $this->log(self::EMERGENCY, $message, $context);
+    }
+
+    /**
+     * Action must be taken immediately
+     *
+     * @param   string      $message    The message
+     * @param   array       $context    The context
+     */
+    public function alert($message, array $context = [])
+    {
+        $this->log(self::ALERT, $message, $context);
+    }
+
+    /**
+     * Critical conditions
+     *
+     * @param   string      $message    The message
+     * @param   array       $context    The context
+     */
+    public function critical($message, array $context = [])
+    {
+        $this->log(self::CRITICAL, $message, $context);
+    }
+
+    /**
+     * Runtime errors that do not require immediate action but should typically be logged and monitored
+     *
+     * @param   string      $message    The message
+     * @param   array       $context    The context
+     */
+    public function error($message, array $context = [])
+    {
+        $this->log(self::ERROR, $message, $context);
+    }
+
+    /**
+     * Exceptional occurrences that are not errors
+     *
+     * @param   string      $message    The message
+     * @param   array       $context    The context
+     */
+    public function warning($message, array $context = [])
+    {
+        $this->log(self::WARNING, $message, $context);
+    }
+
+    /**
+     * Normal but significant events
+     *
+     * @param   string      $message    The message
+     * @param   array       $context    The context
+     */
+    public function notice($message, array $context = [])
+    {
+        $this->log(self::NOTICE, $message, $context);
+    }
+
+    /**
+     * Interesting events
+     *
+     * @param   string      $message    The message
+     * @param   array       $context    The context
+     */
+    public function info($message, array $context = [])
+    {
+        $this->log(self::INFO, $message, $context);
+    }
+
+    /**
+     * Detailed debug information
+     *
+     * @param   string      $message    The message
+     * @param   array       $context    The context
+     */
+    public function debug($message, array $context = [])
+    {
+        $this->log(self::DEBUG, $message, $context);
+    }
+
 }
 
